52b2628e8311349e1508666cc66a374bfefcd49b
Emmanu Varghese <emmanuvarghese@gmail.com>
2026-05-11 17:30:40 +0530

    Update tracks.rb

--- config/initializers/tracks.rb
+++ config/initializers/tracks.rb
@@ -1,3 +1,12 @@
-TRACKS_VERSION='2.7.1'
-TRACKS_REVISION_WITH_DATE=`git log --date=format:'%Y-%m-%d' --pretty=format:"%h @ %ad" -1`
-TRACKS_REVISION=`git log --pretty=format:"%h" -1`
+TRACKS_VERSION = '2.7.1'
+
+if Dir.exist?(".git")
+  TRACKS_REVISION_WITH_DATE =
+    `git log --date=format:'%Y-%m-%d' --pretty=format:"%h @ %ad" -1`.strip
+
+  TRACKS_REVISION =
+    `git log --pretty=format:"%h" -1`.strip
+else
+  TRACKS_REVISION_WITH_DATE = "render-deploy"
+  TRACKS_REVISION = ENV["RENDER_GIT_COMMIT"] || "unknown"
+end
