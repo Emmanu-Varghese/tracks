6060b1713a660aac2da27622ec9ae0f4a25a5355
Emmanu Varghese <emmanuvarghese@gmail.com>
2026-05-12 10:49:03 +0530

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,37 @@
-#!/bin/bash
+#!/bin/sh
+set -e
+
+echo "Starting entrypoint..."
 
 export RAILS_ENV=${RAILS_ENV:-production}
 export DATABASE_NAME=${DATABASE_NAME:-tracks}
 export DATABASE_HOST=${DATABASE_HOST:-db}
 export DATABASE_PORT=${DATABASE_PORT:-3306}
 export DATABASE_USERNAME=${DATABASE_USERNAME:-tracks}
-if [ "$DATABASE_PASSWORD_EMPTY" != 1 ];
-then
+
+if [ "$DATABASE_PASSWORD_EMPTY" != "1" ]; then
   export DATABASE_PASSWORD=${DATABASE_PASSWORD:-password}
 else
   export DATABASE_PASSWORD=""
 fi
+
 export DATABASE_TYPE=${DATABASE_TYPE:-mysql2}
 export DATABASE_ENCODING=${DATABASE_ENCODING:-utf8}
 
 export RAILS_SERVE_STATIC_FILES=TRUE
 export RAILS_LOG_TO_STDOUT=TRUE
+
+echo "Waiting for database..."
+
+until nc -z $DATABASE_HOST $DATABASE_PORT; do
+  echo "Database not ready yet..."
+  sleep 2
+done
+
+echo "Database is ready!"
+
+echo "Running migrations..."
 bundle exec rails db:migrate
 
+echo "Starting Rails server..."
 exec "$@"
